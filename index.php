7d9bc9f72e57a586880bff8cb9164131fac8070a
Erick Rangel <edrangel@bancolombia.com.co>
2022-03-14 23:38:12 -0500

    Adjusted Integration social Networks Icon Login Modal

--- index.php
+++ index.php
@@ -11,28 +11,30 @@ use Hybridauth\Hybridauth;
 $hybridauth = new Hybridauth($config);
 $adapters = $hybridauth->getConnectedAdapters();
 ?>
-<div>
-<ul>
+<div class="d-none">
+<ul >
     <?php foreach ($hybridauth->getProviders() as $name): ?>
         <?php if (!isset($adapters[$name])): ?>
             <li>
-                <a href="<?php print $config['callback'] . "?provider={$name}";?>">
+                <a class="provider" href="<?php print $config['callback'] . "?provider={$name}";?>">
                     Sign in with <strong><?php print $name;?></strong>
                 </a>
             </li>
         <?php endif;?>
     <?php endforeach;?>
 </ul>
-</div>
+
 <?php require_once 'registerSocial.php'?>
 <?php foreach ($adapters as $name => $adapter): ?>
-<?php print_r($adapter->getUserProfile())?>
-<?php echo '' . $name;
+<!-- <?php print_r($adapter->getUserProfile())?> -->
+<?php //echo '' . $name;
 $data = $adapter->getUserProfile();
 $register = new RegisterSocial();
 $register->insertUser($data, $name);
 ?>
+<span>(<a href="<?php print $config['callback'] . "?logout={$name}";?>">Log Out</a>)</span>
 <?php endforeach;?>
+</div>
 
 
 <!DOCTYPE html>
@@ -42,16 +44,17 @@ $register->insertUser($data, $name);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://www.google.com/recaptcha/api.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
     <!-- sidebar social network -->
-    <link type="text/css" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <!-- <link type="text/css" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet"> -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 
     <link rel="stylesheet" type="text/css" href="css/jquery-social-share-bar.css">
@@ -445,8 +448,11 @@ $idTema = $row['idTema'];
                         <input type="password" id="pass" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
                         <span class="invalid-feedback"><?php echo $password_err; ?></span>
                     </div>
-
-                    <a href="#" class="fa fa-facebook"></a>
+                    <div class="mb-3 d-flex justify-content-center" >
+                    <a href="#" onclick="document.getElementById('provider0').click();" class="fa fa-twitter"></a>
+                    <a href="#" onclick="document.getElementById('provider1').click();" class="fa fa-facebook"></a>
+                    <a href="#" onclick="document.getElementById('provider2').click();" class="fa fa-google"></a>
+                    </div>
             </div>
             <div class="modal-footer d-flex justify-content-center">
 
